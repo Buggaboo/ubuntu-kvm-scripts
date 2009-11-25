50544223411c21bc8632cd954eefd74984bf7d3b
jasm <jasm@bmdell-d056.ontwikkel>
2009-11-25 16:51:27 +0100

    changed script to start qemu kvm vm guest

--- scripts/start-virtual-guest-img-tap.sh
+++ scripts/start-virtual-guest-img-tap.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
-if [ -z $1 ] ;then
-  echo "Error: No image set to boot!"
+# 1st parameter: $1 -> image.qcow2
+# 2nd parameter: $2 -> tap0..n
+# 3rd parameter: $3 -> virtual ethernet device
+# 4th parameter: $4 -> additional parameters, enter like this "-usb -usbdevice 1234:5678"
+
+# pass to 4th parameter
+# tip: -nographics
+# tip: -usb -usbdevice PQRS:TUVW where P..W are 1..9
+
+if [ -z "$1" ] ;then
+  echo "Error: No vm image set to boot!"
   exit 1
 fi
 
 MODEL=$3
-if [ -z $3 ]; then
-  echo "Error: No nic model set!"
-  echo "Error: Default will be 'e1000'."
+if [ -z "$3" ]; then
+  echo "Warning: No nic model set!"
+  echo "Warning: Default will be 'e1000'."
   MODEL=e1000
 fi
 
-if [ -z $2 ]; then
+if [ -z "$2" ]; then
   echo "Error: No tap device chosen to connect to virtual guest!"
   exit 1
 fi
 
-if [ -z $4 ]; then
+if [ -z "$4" ]; then
   echo "Warning: No extra parameters given to qemu."
 fi
 
@@ -26,6 +35,12 @@ MACADDR="DE:AD:BE:EF:$N$N:$N$N"
 
 echo "mac address: $MACADDR"
 
-kvm -hda $1 -m 1024 smp 2 $4 \
+KVM=$(which kvm)
+if [ -z "$KVM" ]; then
+  echo "Error: no qemu available."
+  exit 1
+fi
+
+sudo $KVM -hda $1 -m 1024 smp 2 $4 \
   -net nic,macaddr=$MACADDR,model=$MODEL \
-  -net tap,ifname=$2,script=no 
+  -net tap,ifname=$2,script=no
