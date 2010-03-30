7069af5110d35239edffa32107bb4303b42c586c
Buggaboo <buggasnoo@gmail.com>
2010-03-30 03:12:35 +0200

    changed the start-tap.sh, see slackware project like this

--- scripts/start-tap.sh
+++ scripts/start-tap.sh
@@ -3,11 +3,11 @@
 # 1st parameter: $1 -> image.qcow2
 # 2nd parameter: $2 -> tap0..n
 # 3rd parameter: $3 -> virtual ethernet device
-# 4th parameter: $4 -> additional parameters, enter like this "-usb -usbdevice host:1234:5678"
+# 4th parameter: $4 -> additional parameters, enter like this "-usb -usbdevice 1234:5678"
 
 # pass to 4th parameter
 # tip: -nographics
-# tip: -usb -usbdevice host:PQRS:TUVW where P..W are 1..9
+# tip: -usb -usbdevice PQRS:TUVW where P..W are 1..9
 
 if [ -z "$1" ] ;then
   echo "Error: No vm image set to boot!"
@@ -30,13 +30,12 @@ if [ -z "$4" ]; then
   echo "Warning: No extra parameters given to qemu."
 fi
 
-# This sets the mac address of the virtual ethernet device
 N=$(echo -n $2 | sed 's/.*\([0-9]\+\)$/\1/')
 MACADDR="DE:AD:BE:EF:$N$N:$N$N"
 
 echo "mac address: $MACADDR"
 
-KVM=$(which kvm)
+KVM=$(which qemu-system-x86_64)
 if [ -z "$KVM" ]; then
   echo "Error: no qemu available."
   exit 1
@@ -45,4 +44,8 @@ fi
 sudo $KVM -hda $1 -m 1024 -smp 2 $4 \
   -net nic,macaddr=$MACADDR,model=$MODEL \
   -net tap,ifname=$2,script=no \
-  -usb
+  -usb \
+  -name "`basename $1` $2" \
+  -no-quit \
+  -monitor stdio
+  
